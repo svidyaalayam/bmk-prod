ca5a3c58c7b184cc686c244e4a7ddfc66f74a54e
svidyaalayam <svidyaalayam@gmail.com>
2023-12-25 19:39:30 +0000

    jha sorted

--- src/app/component/others/scroll-header/scroll-header.component.ts
+++ src/app/component/others/scroll-header/scroll-header.component.ts
@@ -15,7 +15,7 @@ export class ScrollHeaderComponent implements OnInit {
   constructor(private birthdayService: BirthdaysService) {
     this.scrollingText += '🙏🙏🙏 ఓం శ్రీ మహాగణాధిపతయే నమః 🙏🙏🙏 శ్రీ వాగ్దేవ్యై నమః 🙏🙏🙏 శ్రీ గురుభ్యో నమః 🙏🙏🙏 ';
     this.scrollingText += 'అ ఆ ఇ ఈ ఉ ఊ ఋ ౠ ఌ ౡ ఎ ఏ ఐ ఒ ఓ ఔ అం అః క  ఖ గ ఘ ఙ  చ ౘ ఛ జ ౙ ఝ ఞ ట ఠ డ ఢ ణ త థ ద ధ న ప ఫ బ భ మ య ర ల వ  శ ష స హ ళ క్ష ఱ ';
-    this.scrollingText += '🙏🙏🙏 Please note 15th December 2023 is the last date to register for participating in శ్రీ సరస్వతీ పూజ మరియు సద్గురు శ్రీత్యాగరాజ స్వామి ఆరాధనోత్సవములు. For full details see home page - upcoming events 🙏🙏🙏 ';
+    this.scrollingText += '🙏🙏🙏 Upcoming event : 18th February 2024 శ్రీ సరస్వతీ పూజ మరియు సద్గురు శ్రీత్యాగరాజ స్వామి ఆరాధనోత్సవములు. 🙏🙏🙏 ';
     
 
   }
